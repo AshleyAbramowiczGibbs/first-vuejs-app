880c042ed94ea54e8bbf1c5424220b8054846ea8
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2018-11-15 20:20:39 -0600

    First vuejs app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Add a to do item</h1>
+    <button v-on:click="addToDo()">Add To Do Item!</button>
+    <h1>{{ message }}</h1>
+    <div v-for="todos in todo">
+      <p>{{ todos.text }} </p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      todo: [
+        {
+          id: 1,
+          text: "Thank you notes",
+          complete: false
+        },
+        {
+          id: 2,
+          text: "Run",
+          complete: false
+        },
+        {
+          id: 3,
+          text: "Read my book",
+          complete: false
+        }
+      ]
+    };
+  },
+  created: function() {
+    
+  },
+  methods: {
+    addToDo: function() {
+      let newTask = {
+        id: 4,
+        text: "Work on capstone",
+        complete: false
+      };
+      this.todo.push(newTask);
+    }
+  },
+  computed: {}
+};
 </script>
